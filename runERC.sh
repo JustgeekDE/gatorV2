03a76fe6b0f4b2bb1c384e15157ee0f233a77612
Philip Peter <github@justgeek.de>
2016-04-21 18:29:16 +0200

    adds verbose error message

--- runERC.sh
+++ runERC.sh
@@ -3,6 +3,7 @@
 eagle -C "ERC; run export-error SAVE ercResult.txt; quit" $1
 
 if grep -q "error has not yet been approved" "ercResult.txt"; then
+  cat ercResult.txt
   rc=1
 else
   rm ercResult.txt
