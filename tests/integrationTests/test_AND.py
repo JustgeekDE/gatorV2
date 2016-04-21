cf6eb7c4844d0b96cc624ea33e6575bdb7a69172
Philip Peter <github@justgeek.de>
2016-04-21 18:48:10 +0200

    added more tests for AND logic

--- tests/integrationTests/test_AND.py
+++ tests/integrationTests/test_AND.py
@@ -7,12 +7,45 @@ class ANDTests(ALUTest):
 
   def testLowAndLow(self):
     circuit = self.initCircuit("S_AND")
-    circuit.setSignal(GenericSignal("S_AND", HIGH))
+
     circuit.setSignal(GenericSignal("A", LOW))
     circuit.setSignal(GenericSignal("B", LOW))
 
     circuit.run()
 
     self.expectLow(circuit,"RESULT")
+    self.checkCurrent(circuit)
+
+  def testLowAndHigh(self):
+    circuit = self.initCircuit("S_AND")
+
+    circuit.setSignal(GenericSignal("A", LOW))
+    circuit.setSignal(GenericSignal("B", HIGH))
+
+    circuit.run()
+
+    self.expectLow(circuit,"RESULT")
+    self.checkCurrent(circuit)
+
+  def testHighAndLow(self):
+    circuit = self.initCircuit("S_AND")
+
+    circuit.setSignal(GenericSignal("A", HIGH))
+    circuit.setSignal(GenericSignal("B", LOW))
+
+    circuit.run()
+
+    self.expectLow(circuit,"RESULT")
+    self.checkCurrent(circuit)
+
+  def testHighAndHigh(self):
+    circuit = self.initCircuit("S_AND")
+
+    circuit.setSignal(GenericSignal("A", HIGH))
+    circuit.setSignal(GenericSignal("B", HIGH))
+
+    circuit.run()
+
+    self.expectHigh(circuit,"RESULT")
     self.checkCurrent(circuit)
 
--- tests/integrationTestsSimulation.py
+++ tests/integrationTestsSimulation.py
@@ -6,7 +6,7 @@ from pkg_resources import resource_string
 
 from scoville.circuit import Circuit
 from scoville.eagleSchematic import EagleSchematic
-from integrationTests import test_ADD, test_ID
+from integrationTests import test_ADD, test_ID, test_AND
 
 def getCircuitFunction():
   def getCircuit(self):
@@ -31,5 +31,6 @@ if __name__ == '__main__':
 
   success = success and runTests(test_ADD.AdditionTests)
   success = success and runTests(test_ID.IdentityTests)
+  success = success and runTests(test_AND.ANDTests)
 
   sys.exit(not success)
